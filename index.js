5264b2e234b4f3cd3e5334211419a935951ecb52
Nikolay <freshitftm@gmail.com>
2021-06-23 11:04:44 +0300

    Add create-user request

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const http = require('http')
 const fs = require('fs').promises
 
+let counter = 0
+
+const users = [];
+
 const routerGET = {
   '/': async (req, res) => {
     const data = await fs.readFile('./views/index.html', 'utf8')
@@ -16,10 +20,39 @@ const routerGET = {
   }
 }
 
+const routerPOST = {
+  '*': async (req, res) => {
+    counter++
+    console.log(counter)
+    res.end()
+  },
+  '/create-user': async (req, res) => {
+    let jsonString = ''
+    req.on('data', chunk => {
+      jsonString += chunk
+    })
+    req.on('end', () => {
+      const user = JSON.parse(jsonString);
+      user.id = Date.now();
+      users.push(user);
+      console.log(users);
+      res.end(JSON.stringify(user));
+    })
+  }
+}
+
 const requestListener = async (req, res) => {
   const { url, method } = req
   if (method === 'GET' && routerGET[url]) {
     return routerGET[url](req, res)
+  }
+
+  if (method === 'POST') {
+    if (routerPOST[url]) {
+      return routerPOST[url](req, res)
+    } else {
+      return routerPOST['*'](req, res)
+    }
   }
 
   const data = await fs.readFile('./views/404.html', 'utf8')
